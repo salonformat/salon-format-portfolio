75831e4d6d8321f4a903eb5df8190611df28b62b
salonformat <salonformat@users.noreply.github.com>
2026-09-02 09:24:29 +0200

    Connect custom domain and simplify project URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://salonformat.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/salon-format-portfolio/' : '/',
+  site: 'https://salonformat.com',
+  base: '/',
 });
